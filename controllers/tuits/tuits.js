dec673afb9e23581d075e4765d6977f9d0e65add
Heekyung Kim <heekyung.kim27@gmail.com>
2022-04-07 16:06:56 -0400

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      "_id": "123",
+      "postedBy": {"username": "NASA"},
+      "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+      "stats": {
+        "likes": 345,
+        "comments": 12,
+        "retuits": 202,
+      },
+      "logo_image" : "https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB.jpg",
+
+    },
+    {
+      "_id": "234",
+      "postedBy": { "username": "NASA" },
+      "tuit": "Ingenuity helicopter takes flight.",
+      "stats": {
+        "likes": 32,
+        "comments": 120,
+        "retuits": 20,
+      },
+      "logo_image" : "https://pbs.twimg.com/profile_images/1321163587679784960/0ZxKlEKB.jpg",
+    }
+  ];
+  
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import helloController from './controllers/hello-controller.js';
+import userController from './controllers/user-controller.js';
+import tuitsController from './controllers/tuits-controller.js';
+
+// const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send("Welcome to Full Stack Development!")})
+
+// app.get('/hello', (req, res) => {res.send('Life is Good!')})
+helloController(app);
+userController(app);
+tuitsController(app);
+
+app.listen(process.env.PORT || 4000);
+
+// app.listen(4000);
