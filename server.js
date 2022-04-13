ac3479b00eb4da8e790a4f1bdb7e6c95fbe68cc2
Heekyung Kim <heekyung.kim27@gmail.com>
2022-04-13 17:01:35 -0400

    Refactor DAO

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import cors from 'cors'
 import helloController from './controllers/hello-controller.js';
 import userController from './controllers/user-controller.js';
 import tuitsController from './controllers/tuits-controller.js';
+import mongoose from 'mongoose';
 
 // const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(cors());
+mongoose.connect('mongodb://localhost:27017/webdev');
 
 app.get('/', (req, res) => {res.send("Welcome to Full Stack Development!")})
 
