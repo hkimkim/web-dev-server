81d6fe478a28a49077b92262ff4a877e9f38c05a
Heekyung Kim <heekyung.kim27@gmail.com>
2022-04-14 17:18:10 -0400

    Finish a9-module3

--- controllers/tuits-schema.js
+++ controllers/tuits-schema.js
@@ -1,10 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
     tuit: String,
     likes: Number,
+    comments: {
+        type: Number,
+        default: 0,
+    },
+    retuits: {
+       type: Number,
+       default: 0,
+    },
     postedBy: {
         username: String
+    },
+    logo_image: {
+        type: String,
+        default: "https://pbs.twimg.com/profile_images/1407528509170401280/B7uaH6fC_400x400.jpg",
+    },
+    liked: {
+        type: Boolean,
+        default: false,
     }
 }, {collection: 'tuits'});
 export default schema;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import helloController from './controllers/hello-controller.js';
 import userController from './controllers/user-controller.js';
-import tuitsController from './controllers/tuits-controller.js';
+import tuitsController from './controllers/tuits-controller.js'
 import mongoose from 'mongoose';
 
 // const express = require('express');
